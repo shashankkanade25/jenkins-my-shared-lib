7fb22186a3460375b5e571d9ab3890a6bedde0d8
Shashank kanade <shashankkanade07@gmail.com>
2026-01-09 03:01:11 +0530

    Fix string quotes in docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,6 @@
 def call(String credId , String imageName){
   withCredentials([usernamePassword(
-                    credentialsId: '${credId}',
+                    credentialsId: "${credId}",
                     usernameVariable: 'dockerHubUser',
                     passwordVariable: 'dockerHubPassword'
                 )]) {
