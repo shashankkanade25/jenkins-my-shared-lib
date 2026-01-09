77f7862cd4bb346bccffdb642923c2556c1ae781
Shashank kanade <shashankkanade07@gmail.com>
2026-01-09 18:15:09 +0530

    Fix variable usage in docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -4,12 +4,14 @@ def call(String credId, String imageName) {
     usernameVariable: 'DOCKER_USER',
     passwordVariable: 'DOCKER_PASS'
   )]) {
-    sh '''
-      set -e
+    withEnv(["IMAGE_NAME=${imageName}"]) {
+      sh '''
+        set -e
         echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-        docker tag "$imageName" "$DOCKER_USER/$IMAGE_NAME"
+        docker tag "$IMAGE_NAME" "$DOCKER_USER/$IMAGE_NAME"
         docker push "$DOCKER_USER/$IMAGE_NAME:latest"
         docker logout
-    '''
+      '''
+    }
   }
 }
